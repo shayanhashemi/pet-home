4c2ad39c24bb6a0b2b6d40365685dd4e729ad15e
shayanhashemi <shayanhashemi719@gmail.com>
2024-01-11 22:53:15 +0330

    Adding update_cart funtion.

--- FastApi.py
+++ FastApi.py
@@ -25,3 +25,20 @@ def add_to_cart(user_id: str, item_id: str, quantity: int):
     user_history.append(('add', item_id, quantity))
 
     return {'message': 'Item added to cart successfully'}
+
+
+@app.put('/cart/{user_id}/{item_id}')
+def update_cart(user_id: str, item_id: str, quantity: int):
+    if user_id not in shopping_carts:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_cart = shopping_carts[user_id]
+    user_history = cart_history.setdefault(user_id, deque())
+
+    if item_id not in user_cart:
+        raise HTTPException(status_code=404, detail="Item not found or invalid request")
+
+
+    user_history.append(('update', item_id, quantity))
+
+    return {'message': 'Cart updated successfully'}
